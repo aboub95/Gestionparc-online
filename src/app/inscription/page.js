395199d59c38e789faf4471e09c494eb75cec1ad
thanzou <axelromain74@gmail.com>
2024-10-19 03:33:08 +0100

    formulaire

--- src/app/inscription/page.js
+++ src/app/inscription/page.js
@@ -1,21 +1,23 @@
+import { IMAGES_MANIFEST } from "next/dist/shared/lib/constants";
 import Image from "next/image";
 
 export default function page() {
   return (
     <div>
-       <div className="bg-green-500 max-md:flex-col items-center px-4  h-screen flex justify-evenly">
-           <div className="max-sm:flex-col max-md:mt-14  ">
-             <h1 class="text-2xl text-white font-bold  text-center mb-6">BIENVENUE</h1>
+       <div className="bg-green-500 max-md:flex-col items-center px-4   flex justify-evenly   h-screen bg-cover bg-center" style={{ backgroundImage:`url(garer.jpg)` }} >
+           <div className="bg-blue-500  max-md:h-auto p-6 rounded-lg  h-auto lg:w-5/12 shadow-md max-md:w-full ">
+           <div className="max-sm:flex-col max-md:mt-14" >
+             <h1 class="text-2xl text-white font-bold  text-center fade-in mb-6">BIENVENUE</h1>
              <p class="text-xl text-white font-bold text-center mb-6">Sur votre dashboard</p>
             </div>
-           <div className="bg-white h-100 p-6 rounded-lg shadow-md w-full max-w-md">
              <form>
-               <input type="email" placeholder="Email" required class="border border-gray-300 p-2 rounded w-full mb-4"/>
+               <input type="email" formMethod="post" placeholder="Email" required class="border border-gray-300 p-2 rounded w-full mb-4"/>
                 <input type="password" placeholder="Mot de passe" required class="border border-gray-300 p-2 rounded w-full mb-4"/>
-               <button type="submit" class="bg-green-500 text-white w-1/2 p-2 rounded-3xl ml-20 hover:bg-green-600">Connexion</button>
+               <button type="submit" class="bg-green-500 text-white w-1/2 p-2 rounded-3xl mb-2  hover:bg-green-600">Connexion</button>
                </ form>
+               <Image src="/voiture.gif" alt="My image" width={500} height={300} />
          </div>
-       </div>
+      </div>
     </div>
   );
 }
