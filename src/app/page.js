7080fcb82c1a56936df8a61a39e8731e63f4c089
gatien074 <ngouessygatien@gmail.com>
2024-10-20 11:18:52 +0200

    version4

--- src/app/page.js
+++ src/app/page.js
@@ -5,11 +5,13 @@ import Deuxièmesect from "./Components/Deuxièmesect"
 import Footer from "./Components/Footer"
 import Header from "./Components/Header"
 import Premieresect from "./Components/Premieresect"
+
+
 export default function(){
     return(
         <>
           <Header/>
-          <main className="bg-cover bg-center" style={{ backgroundImage: `url(/photos.png)` }}>
+          <main className="bg-cover bg-center">
     <div className="flex flex-col items-center py-52 px-4 md:px-28">
         <h1 className="text-4xl md:text-5xl text-white text-center mt-4">
             L’application Web de la <br/> 
